c97b55091032c8ed636d379ecf150fc15a02e166
Yoon, Chanhyeok <jomecena26@naver.com>
2024-11-20 09:49:41 +0900

    메세지 Merge branch 'main' into issue2 - merge conflict 해결

--- src/p202014116/Main.java
+++ src/p202014116/Main.java
@@ -9,6 +9,9 @@ public static void main(String[] args) {
 	out.println(Hello.sum(10));
 	out.println(Hello.sum(100));
 	out.println(Hello.sum(1000));
+	out.println(World.factorial(3));
+	out.println(World.factorial(6));
+	out.println(World.factorial(9));
 	out.println(Sansu.max(new int[] {8, 5, 6, 9, 4, 1, 7, 10, 3, 2, 8}));
 	out.println(Sansu.max(new int[] {-2, 5, 6, 9, -6, 1, 7, 2, 8, 10}));
 
