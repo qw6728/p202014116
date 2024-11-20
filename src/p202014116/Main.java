c06f16ae0bdefa32b2da97b8c328ac2582f50616
Yoon, Chanhyeok <jomecena26@naver.com>
2024-11-20 09:27:00 +0900

    메세지 three

--- src/p202014116/Main.java
+++ src/p202014116/Main.java
@@ -6,6 +6,8 @@ public static void main(String[] args) {
 System.out.println("201514099 홍길동");
 System.out.println(Hello.sum(10));
 System.out.println(Hello.sum(100));
+System.out.println(Hello.sum(1000));
+
 
 }
 }
