2dc6e5fcb8e2d71b58eca1d05a01a5650dcef2de
Yoon, Chanhyeok <jomecena26@naver.com>
2024-11-20 09:35:27 +0900

    메세지 four

--- src/p202014116/Main.java
+++ src/p202014116/Main.java
@@ -3,6 +3,12 @@ package p202014116;
 public class Main {
 public static void main(String[] args) {
 // 자신의 학번과 이름을 출력하라
-System.out.println("201514099 홍길동");
+System.out.println("202014116 윤찬혁");
+System.out.println(Hello.sum(10));
+System.out.println(Hello.sum(100));
+System.out.println(Hello.sum(1000));
+System.out.println(World.factorial(3));
+System.out.println(World.factorial(6));
+System.out.println(World.factorial(9));
 }
 }
